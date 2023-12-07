c9ac46f0e13c79238acf17d7212f4a9d0befabd6
jinxin <xin.jin@omnigames.cc>
2023-12-07 19:02:56 +0800

    GameUI

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Rebort
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
